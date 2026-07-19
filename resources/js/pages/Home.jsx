9f58595938e53bb75af3d5c4ce2e4838b9f4dd38
novendra27 <adi.novendra.p@gmail.com>
2026-07-19 13:31:50 +0700

    chore: set up Laravel, Inertia, React, and Tailwind CSS

---
+++ resources/js/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-3xl font-bold text-gray-800">
+                LMS App berjalan dengan baik!
+            </h1>
+        </div>
+    );
+}
